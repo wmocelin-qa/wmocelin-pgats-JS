94222fc968a972600ebbb87246626a116d4b8003
Gisele Mocelin <wesleymocelin@gmail.com>
2025-04-26 14:37:33 -0300

    up

--- Conceitos/funcoes-de-seta.js
+++ Conceitos/funcoes-de-seta.js
@@ -1,60 +1,61 @@
-// funcao nomeada, com parametro, sem retorno
-function exibirNomeDoDog(nome) {
-    console.log(nome)
-  }
-  
-  // funcao nomeada, com parametro + valor default, sem retorno
-  function exibirPorteDoDog(porte = 'ND') {
-    console.log(porte)
-  }
-  
-  // funcao nomeada, com parametro, com retorno
-  function obterNomeDoDogFormatado(nome) {
-    return nome.toUpperCase()
-  }
-  
-  function obterObjetoDog() {
-    return {
-      nome: 'Bailey Maria',
-      peso: 11.5,
-      dogIrmao: {
-        nome: 'Billy'
-      },
-      adotado: true
-    }
-  }
-  
-  function listarObjetosDog() {
-    return [{
-      nome: 'Bailey Maria',
-      peso: 11.5
-    }, {
-      nome: 'Bailey Maria',
-      peso: 11.5
-    }]
-  }
+// função de seta / flecha
 
-  function listarNomesDeDogs() {
-    const nomes = [
-      'Pituca',
-      'Marea',
-      'Anderson'
-    ]
-    
-    console.log(`Passou pela função listarNomesDeDogs`)
-  
-    return nomes
+const exibirNomeDog = (nome) => {
+  console.log(nome)
+}
+
+const exibirPorteDog = (porte = 'ND') => {
+  console.log(porte)
+}
+
+const obterNomeDoDogFormatado = (nome) => {
+  return nome.toUpperCase()
+}
+
+const obterObjetoDog = () => {
+  return {
+    nome: 'Bailey Maria',
+    peso: 11.5,
+    dogIrmao: {
+      nome: 'Billy'
+    },
+    adotado: true
   }
+}
+
+// arrow function sem bloco + return 'automático'
+const dobrar = (numero) => numero * 2
+
+const listarObjetosDog = () => {
+  return [
+    {
+      nome: 'Bailey Maria',
+      peso: 11.5
+    },
+    {
+      nome: 'Bailey Maria',
+      peso: 11.5
+    }
+  ]
+}
+
+const listarNomesDeDogs = () => {
+  const nomes = [
+    'Pituca',
+    'Marea',
+    'Anderson'
+  ]
   
-  exibirNomeDoDog("Pipoca")
-  exibirPorteDoDog()
-  console.log(obterNomeDoDogFormatado("Bailey Maria"))
-  console.log(obterObjetoDog())
-  console.log(listarObjetosDog())
-  console.log(listarNomesDeDogs())
-  
-  console.log(`--------`)
-  
-  listarNomesDeDogs()
-  
-  const nomesDeDogs = listarNomesDeDogs()
+  console.log(`Passou pela função listarNomesDeDogs`)
+  return nomes
+}
+
+// Chamadas corretas das funções
+exibirNomeDog("Pipoca")
+exibirPorteDog()
+console.log(obterNomeDoDogFormatado("Bailey Maria"))
+console.log(obterObjetoDog())
+console.log(listarObjetosDog())
+console.log(listarNomesDeDogs())
+
+console.log(`--------`)
